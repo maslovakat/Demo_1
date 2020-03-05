545c628b9f1a82c68431a12bb2fe0c4a5afe8064
Kateryna <maslova.ekaterina94@gmail.com>
2020-03-05 08:45:37 +0200

    fix task 1

--- js/src/task7.js
+++ js/src/task7.js
@@ -1,19 +1,14 @@
 function getFibonacci(obj) {
-    obj.length = Math.abs(obj.length);
-    obj.min = Math.abs(obj.min);
-    obj.max = Math.abs(obj.max);
-
+    
     // errors
     if(validateFibo(obj)) {
         return validateFibo(obj);
     };
 
     let result = [];
-    let start = Math.pow(10, obj.length - 1) || obj.min;
-    let finish = Math.pow(10, obj.length - 1) * 10 - 1 || obj.max;
+    let start = Math.pow(10, Math.abs(obj.length) - 1) || Math.abs(obj.min);
+    let finish = Math.pow(10, Math.abs(obj.length)) - 1 || Math.abs(obj.max);
     let a, b, c;
-
-    // console.log(start,finish)
 
     if (start === 0) {
         result.push(0, 1);
@@ -29,13 +24,13 @@ function getFibonacci(obj) {
         if (c >= start && c <= finish) {
             result.push(c);
         }
-    } while (c <= finish)
+    } while (c <= finish);
 
     return result;
 }
 
 function validateFibo(obj) {
-    if (!Number.isInteger(obj.length) && obj.length > 0 || !Number.isInteger(obj.min) && obj.min > 0 || !Number.isInteger(obj.max) && obj.max > 0) {
+    if (!Number.isInteger(Math.abs(obj.length)) && Math.abs(obj.length) > 0 || !Number.isInteger(Math.abs(obj.min)) && Math.abs(obj.min) > 0 || !Number.isInteger(Math.abs(obj.max)) && Math.abs(obj.max) > 0) {
         return {
             status: 'failed',
             reason: `Number needs to be whole`
